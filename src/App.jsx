e0fe02e2b9aedf4c9023a6dad4af120dfa9dff2b
Aaron Macandili <11998600+eyeyar03@users.noreply.github.com>
2024-03-18 17:25:36 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,9 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Aaron Macandili",
+  title: "Software Engineer / Java Developer",
+  email: "aaron.macandili.public@google.com",
   gitHub: "microsoft",
   instagram: "microsoft",
   linkedIn: "satyanadella",
